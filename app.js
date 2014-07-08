1e069e70d2b2e2d8d3948635d38d067c3c4ba8d7
Jonathan Matthey <mattheyj@gmail.com>
2014-07-08 02:04:41 -0400

    trying to submit form

--- app.js
+++ app.js
@@ -23,3 +23,111 @@ console.log("Server is running at => http://localhost:" + port + "/\nCTRL + C to
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+var phantom = require('phantom');
+
+phantom.create(function (ph) {
+  ph.createPage(function (page) {
+
+    page.onConsoleMessage = function(msg) {
+      console.log(msg);
+    };
+
+    page.onLoadStarted = function() {
+      loadInProgress = true;
+      console.log("load started");
+    };
+
+    page.onLoadFinished = function() {
+      loadInProgress = false;
+      console.log("load finished");
+    };
+
+        page.open("http://www.citibikenyc.com/login", function (status) {
+          console.log("opened citibikenyc? ", status);
+          setTimeout(function(){
+            page.evaluate(function () {
+
+              var form = document.getElementById("login-form").children[0];
+              var i;
+
+              form.elements["subscriberUsername"].value="mylogin";
+              form.elements["subscriberPassword"].value="mypassword";
+              form.submit();
+
+          setTimeout(function(){
+            return document.title;
+          },2000);
+
+            }, function (result) {
+              console.log('document.getElementById');
+              console.log('result: ' + result);
+              ph.exit();
+            });
+          },2000);
+        });
+
+
+    var steps = [
+    function() {
+        //Load Login Page
+        page.open("http://www.citibikenyc.com/login", function (status) {
+          console.log("opened citibikenyc? ", status);
+          setTimeout(function(){
+            page.evaluate(function () {
+
+              return document.getElementById("login-form");
+              // var form = document.getElementById("login-form").children[0];
+              // var i;
+
+              // form.elements["subscriberUsername"].value="mylogin";
+              // form.elements["subscriberPassword"].value="mypassword";
+              // return ' loggedin?';
+
+            }, function (result) {
+              console.log('document.getElementById');
+              console.log('result: ' + result);
+              ph.exit();
+            });
+          },2000);
+        });
+      },
+      // function() {
+      //   //Login
+      //   page.evaluate(function() {
+      //     var form = document.getElementById("login-form");
+      //     var i;
+
+      //     if (form.getAttribute('method') == "POST") {
+      //       form.submit();
+      //       return;
+      //     }
+
+      //   });
+      // },
+      // function() {
+      //   // Output content of page to stdout after form has been submitted
+      //   page.evaluate(function() {
+      //     console.log(document.querySelectorAll('html')[0].outerHTML);
+      //   });
+      // }
+      ];
+
+
+      // interval = setInterval(function() {
+      //   if (!loadInProgress && typeof steps[testindex] == "function") {
+      //     console.log("step " + (testindex + 1));
+      //     steps[testindex]();
+      //     testindex++;
+      //   }
+      //   if (typeof steps[testindex] != "function") {
+      //     console.log("test complete!");
+      //     ph.exit();
+      //     return;
+      //   }
+      // }, 200);
+
+    });
+});
+
+var testindex = 0, loadInProgress = false;
